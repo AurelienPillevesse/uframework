7ca894a95d9db4ea7ea8366386fc47c498a046b1
Aurelien Pillevesse <aupilleves@dpt_info.iut.local>
2017-02-10 18:25:36 +0100

    update footer

--- app/templates/footer.php
+++ app/templates/footer.php
@@ -22,7 +22,5 @@
     </div>
   </div>
 </footer>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js"></script>
 </body>
 </html>
